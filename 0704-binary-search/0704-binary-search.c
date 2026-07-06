af31c43903ecae0b867027cec22a9b4adb8c7dd2
Anisha Poddar <anishapoddar12@gmail.com>
2026-07-06 18:38:23 +0530

    Time: 0 ms (100%), Space: 10.1 MB (44.75%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,21 @@
+int search(int* nums, int numsSize, int target)
+ {
+    int low=0, high=numsSize-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(target==nums[mid])
+        {
+            return mid;
+        }
+        else if(target > nums[mid])
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+    }
+    return -1;
+}
