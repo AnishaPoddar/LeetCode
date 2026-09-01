9b1bc7c0cd69c2730b677072ee6a7894370ab1b8
Anisha Poddar <anishapoddar12@gmail.com>
2026-09-01 21:31:05 +0530

    Time: 1438 ms (5.03%), Space: 7.8 MB (85.71%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count=0;
+        while(target !=startValue)
+        {
+            if(target > startValue && target %2==0)
+            {
+                target/=2;
+                count++;
+            }
+            else if(target > startValue && target %2!=0)
+            {
+                target++;
+                count++;
+            }
+            else
+            {
+                target++;
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
