42a3aafe42011619647e33943e7d1f84e2729151
Anisha Poddar <anishapoddar12@gmail.com>
2026-09-20 15:30:33 +0530

    Time: 2 ms (32.17%), Space: 42.2 MB (5.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,24 +1,36 @@
 class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int element=nums[0];
+    int count(vector<int>& nums, int element , int low , int high)
+    {
         int count=0;
-        for(int i=0 ; i< nums.size(); i++)
+        for(int i=low ; i<=high ; i++)
         {
-            if(count==0)
-            {
-                element=nums[i];
-            }
             if(nums[i]==element)
             {
                 count++;
             }
-            else
-            {
-                count--;
-            }
         }
-        return element;
-        
+        return count;
+    }
+    int majority(vector<int>& nums, int low, int high)
+    {
+        if(low==high)
+        {
+            return nums[low];
+        }
+        int mid=low+(high-low)/2;
+        int left_max=majority(nums , low , mid);
+        int right_max=majority(nums , mid+1 , high);
+        if(left_max==right_max)
+        {
+            return left_max;
+        }
+        int left_count=count(nums, left_max , low , high);
+        int right_count=count(nums , right_max , low, high);
+
+        return (left_count > right_count) ? left_max : right_max;
+    }
+public:
+    int majorityElement(vector<int>& nums) {
+        return majority( nums, 0 , nums.size()-1);    
     }
 };
