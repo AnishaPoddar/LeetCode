fbf5eddf8750d0cd6cad43ca301fb0f60dae0d54
Anisha Poddar <anishapoddar12@gmail.com>
2026-09-25 00:07:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0 || n==1)
+        if(n<=1)
         {
             return 1;
-        }
-        vector<int> arr(n+1);
-        arr[0]=1;
-        arr[1]=1;
-        for(int i=2; i<=n ; i++)
+        }  
+        int prev=1;
+        int prev2=2;
+        for(int i=3;i<=n; i++)
         {
-            arr[i]=arr[i-1]+arr[i-2];
+            int curr=prev+prev2;
+            prev = prev2;
+            prev2 = curr;
         }
-        return arr[n];   
+        return prev2;
+
     }
 };
